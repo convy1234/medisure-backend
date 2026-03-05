267b81f578f5f7b91e77bbae7bd6ce8d3e9d80ab
john-okojere <johnokojere08@gmail.com>
2026-03-05 14:49:10 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   medisure/settings.py 	modified:   requirements.txt
    Changes not staged for commit:
    	deleted:    .env

--- medisure/settings.py
+++ medisure/settings.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-rbpyn$p_ec3l7y-(_*q+1a^24i1yopl&7&%fi%@vn-a5a!0lvu'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-rbpyn$p_ec3l7y-(_*q+1a^24i1yopl&7&%fi%@vn-a5a!0lvu')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '*').split(',') if host.strip()]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -24,6 +24,10 @@ INSTALLED_APPS = [
     'tracker',
     'rest_framework.authtoken',
 ]
+
+USE_S3 = os.getenv('USE_S3', 'False').lower() == 'true'
+if USE_S3:
+    INSTALLED_APPS.append('storages')
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -57,11 +61,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'medisure.wsgi.application'
 
 DATABASES = {
-    'default': {
+    'default': {}
+}
+
+DB_ENGINE = os.getenv('DB_ENGINE', 'sqlite').lower()
+if DB_ENGINE == 'mysql':
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'medisure'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    }
+else:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -86,8 +106,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -99,7 +117,48 @@ REST_FRAMEWORK = {
 }
 
 BLOCKFROST_PROJECT_ID = os.getenv('BLOCKFROST_PROJECT_ID', '')
-BLOCKFROST_NETWORK = 'testnet'
+BLOCKFROST_NETWORK = os.getenv('BLOCKFROST_NETWORK', 'testnet')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if USE_S3:
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+
+    AWS_STATIC_LOCATION = os.getenv('AWS_STATIC_LOCATION', 'static')
+    AWS_MEDIA_LOCATION = os.getenv('AWS_MEDIA_LOCATION', 'media')
+    AWS_S3_CUSTOM_DOMAIN = os.getenv(
+        'AWS_S3_CUSTOM_DOMAIN',
+        f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    )
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+
+    STORAGES = {
+        'default': {
+            'BACKEND': 'storages.backends.s3.S3Storage',
+            'OPTIONS': {'location': AWS_MEDIA_LOCATION},
+        },
+        'staticfiles': {
+            'BACKEND': 'storages.backends.s3.S3Storage',
+            'OPTIONS': {'location': AWS_STATIC_LOCATION},
+        },
+    }
+else:
+    STATIC_URL = 'static/'
+    MEDIA_URL = 'media/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_ROOT = BASE_DIR / 'media'
+    STORAGES = {
+        'default': {
+            'BACKEND': 'django.core.files.storage.FileSystemStorage',
+        },
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
